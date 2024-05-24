7f31452d4072f6819b5407eb57973f0f4942b8cb
Taiseer Hussein <thussein@redhat.com>
2024-05-24 00:33:54 -0500

    update code

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! baseline');
+  res.send('Hello World! demo');
 });
 
 app.listen(8080, function () {
